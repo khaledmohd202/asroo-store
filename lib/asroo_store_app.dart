566272c1fab9d385852a89d47079b9e7b25bffcb
khaled Mohammad <khaledmohd202@gmail.com>
2024-12-28 11:29:35 +0200

    env created done

--- lib/asroo_store_app.dart
+++ lib/asroo_store_app.dart
@@ -1,3 +1,4 @@
+import 'package:asroo/core/app/env.variables.dart';
 import 'package:flutter/material.dart';
 
 class AsrooStoreApp extends StatelessWidget {
@@ -6,7 +7,7 @@ class AsrooStoreApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: EnvVariables.instance.debugMode,
       title: 'Asroo Store',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,15 @@
 import 'dart:io';
 
 import 'package:asroo/asroo_store_app.dart';
+import 'package:asroo/core/app/env.variables.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvVariables.instance.init(envType: EnvTypeEnum.dev);
 
   Platform.isAndroid
       ? await Firebase.initializeApp(
@@ -19,5 +23,10 @@ void main() async {
           ),
         )
       : await Firebase.initializeApp();
-  runApp(const AsrooStoreApp());
+
+  await SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitDown, DeviceOrientation.portraitUp],
+  ).then((_) {
+    runApp(const AsrooStoreApp());
+  });
 }
