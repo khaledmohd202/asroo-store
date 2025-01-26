ad0507583fa0c5375f68d17f88099717b3b931f0
khaled Mohammad <khaledmohd202@gmail.com>
2025-01-26 07:52:31 +0200

    Start App

---
+++ lib/asroo_store_app.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class AsrooStoreApp extends StatelessWidget {
+  const AsrooStoreApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Asroo Store',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Asroo Store'),
+        ),
+      ),
+    );
+  }
+}
