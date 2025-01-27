d15311cb8e563a567261d574e917b2ec2db95c1a
khaled Mohammad <khaledmohd202@gmail.com>
2025-01-27 19:53:19 +0200

    Please commit your changes or stash them before you switch branches.

--- lib/asroo_store_app.dart
+++ lib/asroo_store_app.dart
@@ -1,4 +1,6 @@
+import 'package:asroo_store/core/app/connectivity_controller.dart';
 import 'package:asroo_store/core/app/env.variables.dart';
+import 'package:asroo_store/core/common/screens/no_network_screen.dart';
 import 'package:flutter/material.dart';
 
 class AsrooStoreApp extends StatelessWidget {
@@ -7,18 +9,39 @@ class AsrooStoreApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Asroo Store',
-      debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Asroo Store'),
-        ),
-      ),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, _) {
+        if (value) {
+          return MaterialApp(
+            title: 'Asroo Store',
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            builder: (context, widget) {
+              return Scaffold(
+                body: Builder(
+                  builder: (context) {
+                    ConnectivityController.instance.init();
+                    return widget!;
+                  },
+                ),
+              );
+            },
+            home: Scaffold(appBar: AppBar(title: const Text('Asroo Store'))),
+          );
+        } else {
+          return MaterialApp(
+            title: 'No Network',
+
+            debugShowCheckedModeBanner: EnvVariable.instance.debugMode,
+
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
   }
 }
