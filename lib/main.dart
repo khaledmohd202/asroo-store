885f0ef3e70c31a4aa0f307b91257f76b546a58a
khaled Mohammad <khaledmohd202@gmail.com>
2025-02-04 03:08:53 +0200

    Setup freezed, flutter Bloc and SharedPreference

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,12 @@
 import 'dart:io';
 
 import 'package:asroo_store/asroo_store_app.dart';
+import 'package:asroo_store/core/app/bloc_observer.dart';
 import 'package:asroo_store/core/app/env.variables.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -25,6 +27,9 @@ void main() async {
         ),
       )
       : Firebase.initializeApp();
+
+  Bloc.observer = AppBlocObserver();
+
   await SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitDown,
     DeviceOrientation.portraitUp,
