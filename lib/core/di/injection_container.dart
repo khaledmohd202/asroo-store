9db5f09e81790a89de37189319987e7a2239fa22
khaled Mohammad <khaledmohd202@gmail.com>
2025-02-05 21:28:16 +0200

    Dio Factory done

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,4 +1,6 @@
 import 'package:asroo_store/core/app/app_cubit/app_cubit.dart';
+import 'package:asroo_store/core/service/graphql/api_service.dart';
+import 'package:asroo_store/core/service/graphql/dio_factory.dart';
 import 'package:get_it/get_it.dart';
 
 final sl = GetIt.instance;
@@ -7,6 +9,9 @@ Future<void> setupInjector() async {
   await _initCore();
 }
 
-Future <void> _initCore() async{
-  sl.registerFactory(AppCubit.new);
+Future<void> _initCore() async {
+  final dio = DioFactory.getDio();
+  sl
+    ..registerFactory(AppCubit.new)
+    ..registerLazySingleton<ApiService>(() => ApiService(dio));
 }
