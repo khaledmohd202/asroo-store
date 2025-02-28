fd30b6b48ec657a1d4295aae360799a8937d3564
khaled Mohammad <khaledmohd202@gmail.com>
2025-02-28 15:00:05 +0200

    Dashboard Bloc

---
+++ lib/features/admin/dashboard/presentation/bloc/users_number/users_number_bloc.dart
@@ -0,0 +1,32 @@
+import 'dart:async';
+import 'package:asroo_store/features/admin/dashboard/data/repos/dashboard_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'users_number_event.dart';
+part 'users_number_state.dart';
+part 'users_number_bloc.freezed.dart';
+
+class UsersNumberBloc extends Bloc<UsersNumberEvent, UsersNumberState> {
+  UsersNumberBloc(this._repo) : super(const UsersNumberState.loading()) {
+    on<GetUsersNumberEvent>(getUsersNumber);
+  }
+
+  final DashboardRepo _repo;
+
+  FutureOr<void> getUsersNumber(
+    GetUsersNumberEvent event,
+    Emitter<UsersNumberState> emit,
+  ) async {
+    final result = await _repo.numberOfUsers();
+
+    result.when(
+      success: (usersData) {
+        emit(UsersNumberState.success(numbers: usersData.usersNumber));
+      },
+      failure: (error) {
+        emit(UsersNumberState.error(error: error));
+      },
+    );
+  }
+}
