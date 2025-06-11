1dd608775c6cf4a56e41fa3fcde3c69663fc709e
khaled Mohammad <khaledmohd202@gmail.com>
2025-06-11 19:56:02 +0300

    Hive Setup

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:asroo_store/asroo_store_app.dart';
 import 'package:asroo_store/core/app/bloc_observer.dart';
 import 'package:asroo_store/core/app/env.variables.dart';
 import 'package:asroo_store/core/di/injection_container.dart';
+import 'package:asroo_store/core/service/hive/hive_database.dart';
 import 'package:asroo_store/core/service/push_notification/firebase_cloud_messaging.dart';
 import 'package:asroo_store/core/service/shared_pref/shared_pref.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -16,7 +17,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EnvVariable.instance.init(envType: EnvTypeEnum.dev);
-Platform.isAndroid
+  Platform.isAndroid
       ? await Firebase.initializeApp(
         options: const FirebaseOptions(
           apiKey:
@@ -35,6 +36,8 @@ Platform.isAndroid
   await setupInjector();
 
   await FirebaseCloudMessaging().initNotification();
+
+  await HiveDatabase().setup();
 
   Bloc.observer = AppBlocObserver();
 
