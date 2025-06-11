a75c435f087f4983a0db8a3edf177d50cbefdac5
khaled Mohammad <khaledmohd202@gmail.com>
2025-06-11 16:18:30 +0300

    Get all Notification item UI

--- lib/features/admin/add_notification/presentation/factors/add_notification_body.dart
+++ lib/features/admin/add_notification/presentation/factors/add_notification_body.dart
@@ -1,3 +1,4 @@
+import 'package:asroo_store/features/admin/add_notification/presentation/widgets/add_notification_item.dart';
 import 'package:asroo_store/features/admin/add_notification/presentation/widgets/create/create_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -9,10 +10,31 @@ class AddNotificationBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsetsGeometry.symmetric(horizontal: 15.w, vertical: 20.h),
-      child: const Column(
+      child: Column(
         children: [
           // Create Notification Title.
-          CreateNotifications(),
+          const CreateNotifications(),
+          // Get All Notifications List.
+          Expanded(
+            child: CustomScrollView(
+              slivers: [
+                SliverToBoxAdapter(child: SizedBox(height: 20.h)),
+                SliverToBoxAdapter(
+                  child: ListView.separated(
+                    itemBuilder: (context, index) {
+                      return AddNotificationItem();
+                    },
+                    separatorBuilder: (context, index) {
+                      return SizedBox(height: 15.h);
+                    },
+                    itemCount: 3,
+                    shrinkWrap: true,
+                    physics: const NeverScrollableScrollPhysics(),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ],
       ),
     );
---
+++ lib/features/admin/add_notification/presentation/widgets/add_notification_item.dart
@@ -0,0 +1,86 @@
+import 'package:asroo_store/core/common/widgets/custom_container_linear_admin.dart';
+import 'package:asroo_store/core/common/widgets/text_app.dart';
+import 'package:asroo_store/core/extensions/context_extension.dart';
+import 'package:asroo_store/core/style/colors/dark_colors.dart';
+import 'package:asroo_store/core/style/fonts/font_family_helper.dart';
+import 'package:asroo_store/core/style/fonts/font_weight_helper.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class AddNotificationItem extends StatelessWidget {
+  const AddNotificationItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomContainerLinearAdmin(
+      height: 180.h,
+      width: MediaQuery.of(context).size.width,
+      child: Padding(
+        padding: EdgeInsetsGeometry.symmetric(horizontal: 10.w, vertical: 15.h),
+        child: Column(
+          children: [
+            const Spacer(),
+            // Title
+            const NotificationInfo(title: 'Title:', body: 'collection.'),
+            const Spacer(),
+            // Body
+            const NotificationInfo(title: 'Body:', body: 'collection.'),
+            const Spacer(),
+            // Date
+            const NotificationInfo(title: 'Create at:', body: '2023-10-01'),
+            const Spacer(),
+            // Edit Buttons
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                // delete button
+                const Icon(Icons.delete, color: Colors.red),
+                SizedBox(width: 40.w),
+                // edit button
+                const Icon(Icons.edit, color: Colors.white),
+                SizedBox(width: 40.w),
+                // Send Button
+                const Icon(Icons.send, color: DarkColors.blueLight),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NotificationInfo extends StatelessWidget {
+  const NotificationInfo({required this.title, required this.body, super.key});
+  final String title;
+  final String body;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        TextApp(
+          text: title,
+          theme: context.textStyle.copyWith(
+            fontSize: 14.sp,
+            color: Colors.white,
+            fontFamily: FontFamilyHelper.poppinsEnglish,
+            fontWeight: FontWeightHelper.medium,
+          ),
+        ),
+        SizedBox(width: 8.w),
+        Flexible(
+          child: TextApp(
+            text: body,
+            theme: context.textStyle.copyWith(
+              color: DarkColors.blueLight,
+              fontSize: 14.sp,
+              fontFamily: FontFamilyHelper.poppinsEnglish,
+              fontWeight: FontWeightHelper.medium,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
