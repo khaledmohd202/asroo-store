bd5e83e3038916ce90dfb79e86d5af4b348ef54d
khaled Mohammad <khaledmohd202@gmail.com>
2025-06-17 11:21:08 +0300

    User Info Data Source, Bloc & Logic

--- lib/features/customer/profile/presentation/refactors/profile_body.dart
+++ lib/features/customer/profile/presentation/refactors/profile_body.dart
@@ -1,5 +1,8 @@
+import 'package:asroo_store/features/customer/profile/presentation/bloc/bloc/profile_bloc.dart';
 import 'package:asroo_store/features/customer/profile/presentation/widgets/user_profile_info.dart';
+import 'package:asroo_store/features/customer/profile/presentation/widgets/user_profile_shimmer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class ProfileBody extends StatelessWidget {
@@ -8,12 +11,24 @@ class ProfileBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: EdgeInsetsGeometry.symmetric(horizontal: 15.w, vertical: 10.h,),
+      padding: EdgeInsetsGeometry.symmetric(horizontal: 15.w, vertical: 10.h),
       child: SingleChildScrollView(
         child: Column(
           children: [
             // User Profile Information.
-            UserProfileInfo(),
+            Center(
+              child: BlocBuilder<ProfileBloc, ProfileState>(
+                builder: (context, state) {
+                  return state.when(
+                    loading: UserProfileShimmer.new,
+                    success:
+                        (userInfoData) =>
+                            UserProfileInfo(userInfo: userInfoData),
+                    error: Text.new,
+                  );
+                },
+              ),
+            ),
           ],
         ),
       ),
--- lib/features/customer/profile/presentation/widgets/user_profile_info.dart
+++ lib/features/customer/profile/presentation/widgets/user_profile_info.dart
@@ -2,12 +2,15 @@ import 'package:asroo_store/core/common/widgets/text_app.dart';
 import 'package:asroo_store/core/extensions/context_extension.dart';
 import 'package:asroo_store/core/extensions/string_extension.dart';
 import 'package:asroo_store/core/style/fonts/font_weight_helper.dart';
+import 'package:asroo_store/features/auth/data/models/user_role_response.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class UserProfileInfo extends StatelessWidget {
-  const UserProfileInfo({super.key});
+  const UserProfileInfo({required this.userInfo, super.key});
+
+  final UserRoleResponse userInfo;
 
   @override
   Widget build(BuildContext context) {
@@ -20,8 +23,7 @@ class UserProfileInfo extends StatelessWidget {
             height: 100.h,
             width: 100.w,
             fit: BoxFit.cover,
-            imageUrl:
-                'https://images.unsplash.com/photo-1749482592769-06ebd3ce7fc8?q=80&w=685&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+            imageUrl: userInfo.userImage!,
             errorWidget: (context, url, error) {
               return Icon(
                 Icons.error_outlined,
@@ -34,7 +36,7 @@ class UserProfileInfo extends StatelessWidget {
         SizedBox(height: 10.h),
         // User Name.
         TextApp(
-          text: 'Khaled'.toLowerCase().toCapitalized(),
+          text: userInfo.userName!.toLowerCase().toCapitalized(),
           theme: context.textStyle.copyWith(
             fontSize: 20.sp,
             fontWeight: FontWeightHelper.bold,
@@ -43,7 +45,7 @@ class UserProfileInfo extends StatelessWidget {
         SizedBox(height: 5.h),
         // User E-mail.
         TextApp(
-          text: 'Khaled@gmail.com',
+          text: userInfo.userEmail!,
           theme: context.textStyle.copyWith(
             fontSize: 14.sp,
             fontWeight: FontWeightHelper.regular,
