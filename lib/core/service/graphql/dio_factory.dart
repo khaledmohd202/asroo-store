1aec1b8673f3930f3fc6d3933761243e47c3cdc4
khaled Mohammad <khaledmohd202@gmail.com>
2025-06-17 11:50:07 +0300

    Status Code 400 Handler

--- lib/core/service/graphql/dio_factory.dart
+++ lib/core/service/graphql/dio_factory.dart
@@ -1,5 +1,6 @@
 import 'package:asroo_store/core/service/shared_pref/pref_keys.dart';
 import 'package:asroo_store/core/service/shared_pref/shared_pref.dart';
+import 'package:asroo_store/core/utils/app_logout.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
@@ -20,8 +21,7 @@ class DioFactory {
             'Bearer ${SharedPref().getString(PrefKeys.accessToken)}';
 
       debugPrint(
-        "[USER Token] ====> ${SharedPref().getString(PrefKeys.accessToken)
-         ?? 'NULL TOKEN'}",
+        "[USER Token] ====> ${SharedPref().getString(PrefKeys.accessToken) ?? 'NULL TOKEN'}",
       );
 
       addDioInterceptor();
@@ -34,5 +34,21 @@ class DioFactory {
 
   static void addDioInterceptor() {
     dio?.interceptors.add(PrettyDioLogger(request: false, compact: false));
+    
+    dio?.interceptors.add(
+      InterceptorsWrapper(
+        onRequest: (options, handler) {
+          options.headers['Authorization'] =
+              'Bearer ${SharedPref().getString(PrefKeys.accessToken)}';
+
+          return handler.next(options);
+        },
+        onError: (error, handler) async {
+          if (error.response?.statusCode == 400) {
+            await AppLogout().logout();
+          }
+        },
+      ),
+    );
   }
 }
