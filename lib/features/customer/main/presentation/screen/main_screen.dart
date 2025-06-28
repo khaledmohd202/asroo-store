11c71e49afe4267af4f7829931f2bb6fdf097037
khaled Mohammad <khaledmohd202@gmail.com>
2025-06-29 00:11:28 +0300

    Favorites Cubit

--- lib/features/customer/main/presentation/screen/main_screen.dart
+++ lib/features/customer/main/presentation/screen/main_screen.dart
@@ -1,7 +1,7 @@
 import 'package:asroo_store/core/enum/nav_bar_enum.dart';
 import 'package:asroo_store/core/extensions/context_extension.dart';
 import 'package:asroo_store/features/customer/categories/presentation/screen/categories_screen.dart';
-import 'package:asroo_store/features/customer/favorites/favorites_screen.dart';
+import 'package:asroo_store/features/customer/favorites/presentation/screen/favorites_screen.dart';
 import 'package:asroo_store/features/customer/home/presentation/screen/home_screen.dart';
 import 'package:asroo_store/features/customer/main/presentation/cubit/main/main_cubit.dart';
 import 'package:asroo_store/features/customer/main/presentation/refactors/main_customer_app_bar.dart';
