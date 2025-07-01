3bb16e15a48fdab13fe5dd245cb5e3ee726a20b0
khaled Mohammad <khaledmohd202@gmail.com>
2025-07-01 08:40:33 +0300

    Share Cubit

---
+++ lib/core/app/share/share_cubit.dart
@@ -0,0 +1,27 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'share_state.dart';
+part 'share_cubit.freezed.dart';
+
+class ShareCubit extends Cubit<ShareState> {
+  ShareCubit() : super(const ShareState.initial());
+
+  Future<void> sendDynamicLinkProduct({
+    required String imageUrl,
+    required String title,
+    required int productId,
+  }) async {
+    emit(ShareState.loading(productId: productId));
+
+    // await DynamicLink()
+    //     .generateProductLink(
+    //       imageUrl: imageUrl,
+    //       productId: productId,
+    //       title: title,
+    //     )
+    //     .then(Share.share);
+
+    emit(const ShareState.success());
+  }
+}
